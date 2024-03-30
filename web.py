b53ce50429b14d08e51dd455b765303a61989bf4
JayaAbirami <151487010+JayaAbirami@users.noreply.github.com>
2024-03-30 20:55:46 +0530

    Add files via upload

---
+++ web.py
@@ -0,0 +1,64 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+
+<body style="background-color:rgba(235, 43, 171, 0.686); height: 100vh;">
+    <div style="display:flex; flex-direction: row; align-items:center; justify-items:center; width:100%; height: 100%;">
+      <div class="card" style=" width: 25rem; margin:13px ">
+        <img src="https://ichef.bbci.co.uk/news/800/cpsprodpb/vivo/live/images/2024/3/5/3c442942-a9df-4595-96b8-46d82ee3086b.jpg" class="card-img-top" alt="...">
+        <div class="card-body">
+          <h5 class="card-title">Latest football news: Reaction as Arsenal thrash Sheffield United </h5>
+          <p class="card-text"> Latest football news and reaction to Sheffield United's 6-0 defeat by Arsenal in the Premier League.</p>
+          <a href="https://www.bbc.com/sport/football/live/cv2yerd4n15t">Read More</a>
+        </div>
+      </div>
+
+      <div class="card" style=" width: 25rem; margin:13px">
+
+        <img src="https://ichef.bbci.co.uk/news/976/cpsprodpb/138A1/production/_132833008_957762679f24af715b91ac1138dc0d26f07eff05.jpg.webp" class="card-img-top" alt="...">
+        <div class="card-body">
+          <h5 class="card-title">Anant Ambani's pre-wedding: Rihanna, Gates and Zuckerberg at India tycoon's gala </h5>
+          <p class="card-text"> Anant Ambani and his wife-to-be Radhika Merchant's three day extravaganza saw Bollywood royalty rubbing shoulders with tech billionaires, sports superstars and even Donald Trump's daughter, Ivanka.          </p>
+          <a href="https://www.bbc.com/news/world-asia-india-68444994">Read More</a>
+        </div>
+      </div>
+      <div class="card" style=" width: 25rem; margin:13px">
+
+        <img src="https://ichef.bbci.co.uk/news/976/cpsprodpb/0D23/production/_132836330_gettyimages-2046568277.jpg.webp" class="card-img-top" alt="...">
+        <div class="card-body">
+          <h5 class="card-title">US Supreme Court rules Colorado cannot ban Trump from presidential ballot </h5>
+          <p class="card-text"> The unanimous ruling is specific to Colorado, but it also overrides challenges brought in other states.
+
+            Colorado had barred Mr Trump from its Republican primary, arguing he incited the 2021 Capitol riot.
+            
+            The court ruled that only Congress, rather than the states, has that power.
+            
+            The top court's decision clears the way for Mr Trump to compete in the Colorado primary scheduled for Tuesday.</p>
+          <a href="https://www.bbc.com/news/world-us-canada-68280062">Read More</a>
+        </div>
+      </div>
+      <div class="card" style=" width: 25rem; margin:13px">
+
+        <img src="https://ichef.bbci.co.uk/news/976/cpsprodpb/1318F/production/_132832287_meier_tcs.jpg.webp" class="card-img-top" alt="...">
+        <div class="card-body">
+          <h5 class="card-title">Christian Meier: From Tour de France grinder to ultra-running winner
+            Published
+            10 hours ago</h5>
+          <p class="card-text"> It is early spring 2021 in Girona, Spain in the middle of yet another Covid-19 lockdown.
+
+            Exercise is permitted, but only in the local municipality.
+            
+            A recent retiree is, like millions of people all over the globe, taking his first steps into running in a bid to break the boredom.
+            
+            He's been riding his bike locally but is fed up with just pedalling "around the block".</p>
+          <a href="https://www.bbc.com/sport/cycling/68427968">Read More</a>
+        </div>
+      </div>
+    </div>
+</body>
+</html>
